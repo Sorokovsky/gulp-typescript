1d26eaeccc116da28bcc761ed4965b6df74093a9
Sorokovsky <Sorokovskys@ukr.net>
2023-01-10 13:21:55 +0300

    tsconfig

--- src/js/modules/variables.js
+++ src/js/modules/variables.js
@@ -1,1 +1,1 @@
-export var header = document.querySelector('.header'), burger = document.querySelector('.burger'), menu = document.querySelector('.menu');
+export let header = document.querySelector('.header'), burger = document.querySelector('.burger'), menu = document.querySelector('.menu');
